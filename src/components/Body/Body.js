0d0461b7320aa1a12eddd0f1344c132b1e7532ef
sundholmm <oskar.sundholm@protonmail.com>
2019-09-17 20:31:15 +0300

    added body text

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import data from '../../data/data.json'
 import './Body.css'
 
 const Body = (props) => {
 
+    const bodyText = data.body.map((paragraph) =>
+        <p>{paragraph}</p>
+    );
+
+    const services = data.body.map((service) =>
+        <li>{service}</li>
+    );
+
     return (
         <div className="body">
-            
+            <div className="body-flex-container">
+                <div>
+                    {bodyText}
+                </div>
+                <div>
+                    {services}
+                </div>
+            </div>
         </div>
     );
 };
