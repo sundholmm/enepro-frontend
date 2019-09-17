6d3132fd916a51b5eac36be4949d3400c3b80628
sundholmm <oskar.sundholm@protonmail.com>
2019-09-17 19:32:37 +0300

    added footer images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,19 @@ const Footer = (props) => {
 
     return (
         <div className="footer">
-            &copy; {date.getFullYear()} EnePro Oy. Kaikki oikeudet pidätetään.
+            <div className="footer-flex-container">
+                <div>
+                    <img className="footer-img" alt="Mitsubishi Electric"
+                    src={process.env.PUBLIC_URL + '/mitsubishi-electric-logo.png'} />
+                </div>
+                <div>
+                    <img className="footer-img" alt="Scanoffice Aurinkopartneri"
+                    src={process.env.PUBLIC_URL + '/so-aurinkopartneri-logo.jpg'} />
+                </div>
+            </div>
+            <p>
+                &copy; {date.getFullYear()} EnePro Oy. Kaikki oikeudet pidätetään.
+            </p>
         </div>
     );
 };
