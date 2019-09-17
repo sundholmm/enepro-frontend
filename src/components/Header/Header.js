95af8a971ca3f320958d350ecd2130666e0a0a7d
sundholmm <oskar.sundholm@protonmail.com>
2019-09-17 19:57:28 +0300

    added header images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,12 @@ const Header = (props) => {
 
     return (
         <div className="header">
-            
+            <div className="header-flex-container">
+                <img className="header-img-logo" alt="EnePro Oy"
+                src={process.env.PUBLIC_URL + '/logo.png'} />
+                <img className="header-img-panels" alt="Paneelikuva"
+                src={process.env.PUBLIC_URL + '/paneelikuva.jpg'} />
+            </div>
         </div>
     );
 };
