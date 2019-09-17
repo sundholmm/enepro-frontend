dd0b0d8252d5f61765417bff4912ce9db4decf48
sundholmm <oskar.sundholm@protonmail.com>
2019-09-17 21:11:02 +0300

    added employee pictures to body

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -4,13 +4,16 @@ import './Body.css'
 
 const Body = (props) => {
 
-    const bodyText = data.body.map((paragraph) =>
-        <p>{paragraph}</p>
+    const bodyText = data.body.map((paragraph, index) =>
+        <p key={index}>{paragraph}</p>
     );
 
-    const services = data.body.map((service) =>
-        <li>{service}</li>
+    const services = data.body.map((service, index) =>
+        <li key={index}>{service}</li>
     );
+
+    const matias = data.employees.matias;
+    const albert = data.employees.albert;
 
     return (
         <div className="body">
@@ -20,6 +23,24 @@ const Body = (props) => {
                 </div>
                 <div>
                     {services}
+                </div>
+                <div className="body-employees-flex-container">
+                    <div className="body-employee-div">
+                        <img className="body-employee-imgs" alt="Matias Virtanen"
+                        src={process.env.PUBLIC_URL + '/matias.jpg'} />
+                        <h2>{matias.firstname + ' ' + matias.lastname}</h2>
+                        <p>{matias.title}</p>
+                        <p>{matias.phone}</p>
+                        <p>{matias.email}</p>
+                    </div>
+                    <div className="body-employee-div">
+                        <img className="body-employee-imgs" alt="Albert Nordström"
+                        src={process.env.PUBLIC_URL + '/albert.jpg'} />
+                        <h2>{albert.firstname + ' ' + albert.lastname}</h2>
+                        <p>{albert.title}</p>
+                        <p>{albert.phone}</p>
+                        <p>{albert.email}</p>
+                    </div>
                 </div>
             </div>
         </div>
