ca166771bfce46669d5a4985ffdf311303ef3da2
sundholmm <oskar.sundholm@protonmail.com>
2019-11-27 18:25:15 +0200

    added title to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,18 @@ const Header = props => {
     <div className="header">
       <div className="header-flex-container">
         <div className="header-img-logo-wrapper">
-          <img
-            className="header-img-logo"
-            draggable="false"
-            alt="EnePro Oy"
-            src={process.env.PUBLIC_URL + "/logo.png"}
-          />
+          <div className="header-img-wrapper">
+            <img
+              className="header-img-logo"
+              draggable="false"
+              alt="EnePro Oy"
+              src={process.env.PUBLIC_URL + "/logo.png"}
+            />
+            <div className="header-title-centered">
+              <h1>EnePro Oy</h1>
+              <h2>Nykyaikaiset energiaratkaisut ammattitaidolla</h2>
+            </div>
+          </div>
         </div>
       </div>
     </div>
