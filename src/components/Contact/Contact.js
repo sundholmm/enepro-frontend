0292c344131331308521db56b07f669bbc2b19e9
sundholmm <oskar.sundholm@protonmail.com>
2020-02-26 14:43:19 +0200

    added axios http post request functionality

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,49 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Contact.css";
 
 const Contact = props => {
   const [emailValue, setEmailValue] = useState("");
   const [phoneValue, setPhoneValue] = useState("");
   const [textValue, setTextValue] = useState("");
+  const [statusValue, setStatusValue] = useState("");
+
+  const clearValues = () => {
+    document.getElementById("contact-form-element").reset();
+    setEmailValue("");
+    setPhoneValue("");
+    setTextValue("");
+  };
 
   const onSubmitHandler = event => {
     event.preventDefault();
-    console.log(emailValue, phoneValue, textValue);
+    let url;
+    if (process.env.NODE_ENV === "development") {
+      url = process.env.REACT_APP_DEVELOPMENT_URL;
+    } else {
+      url = process.env.REACT_APP_PRODUCTION_URL;
+    }
+
+    if (emailValue || phoneValue) {
+      axios
+        .post(url, {
+          email: emailValue,
+          phone: phoneValue,
+          text: textValue
+        })
+        .then(function(response) {
+          clearValues();
+          setStatusValue("success");
+          console.log(response);
+        })
+        .catch(function(error) {
+          clearValues();
+          setStatusValue("error");
+          console.error(error);
+        });
+    } else {
+      setStatusValue("missing");
+    }
   };
 
   return (
@@ -18,7 +53,7 @@ const Contact = props => {
         <h3 className="contact-form-title">{props.title[1]}</h3>
       </div>
       <div className="contact-form">
-        <form onSubmit={onSubmitHandler}>
+        <form onSubmit={onSubmitHandler} id="contact-form-element">
           <div className="contact-form-inner-wrapper">
             <textarea
               className="contact-form-input-text"
@@ -40,6 +75,21 @@ const Contact = props => {
             <button className="contact-form-button">Lähetä</button>
           </div>
         </form>
+        <div className="contact-form-status-wrapper">
+          {statusValue === "success" ? (
+            <div className="contact-form-status-value-success">
+              Lähetys onnistui!
+            </div>
+          ) : statusValue === "error" ? (
+            <div className="contact-form-status-value-error">
+              Lähetys epäonnistui, yritäthän myöhemmin uudelleen!
+            </div>
+          ) : statusValue === "missing" ? (
+            <div className="contact-form-status-value-error">
+              Täytä joko sähköposti tai puhelinnumero!
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
