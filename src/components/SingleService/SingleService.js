0a8d4dfe53207494306af02896ab5f4e99b23c64
sundholmm <oskar.sundholm@protonmail.com>
2020-03-10 21:02:56 +0200

    added single service logic

--- src/components/SingleService/SingleService.js
+++ src/components/SingleService/SingleService.js
@@ -1,9 +1,13 @@
 import React from "react";
 import HeaderLogo from "../Header/HeaderLogo";
+import Footer from "../Footer/Footer";
+import { Link } from "react-router-dom";
 import "./SingleService.css";
 
 const SingleService = props => {
-  const { text } = props.service;
+  const {
+    service: { title, text }
+  } = props;
 
   const singleServiceText = text.map((paragraph, index) => (
     <p key={index}>{paragraph}</p>
@@ -12,8 +16,16 @@ const SingleService = props => {
   return (
     <div className="single-service">
       <HeaderLogo includeTitleAndButton={false} />
-      {props.service.title}
-      {singleServiceText}
+      <div className="single-service-body">
+        <div className="single-service-inner-body">
+          <h2>{title}</h2>
+          {singleServiceText}
+          <Link className="single-service-home-link" to={"/"}>
+            <p>Palaa takaisin etusivulle.</p>
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
