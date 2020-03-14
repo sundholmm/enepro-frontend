92e8c213e61456fa495c79c06985161c3363d65a
sundholmm <oskar.sundholm@protonmail.com>
2020-03-14 12:17:32 +0200

    added links to services

--- src/components/SingleService/SingleService.js
+++ src/components/SingleService/SingleService.js
@@ -6,29 +6,57 @@ import "./SingleService.css";
 
 const SingleService = props => {
   const {
-    service: { title, text, image }
+    service: { title, text, image, details }
   } = props;
 
   const singleServiceText = text.map((paragraph, index) => (
     <p key={index}>{paragraph}</p>
   ));
 
+  const links = details.detailLinks.map((link, index) => (
+    <React.Fragment key={index}>
+      <a
+        href={link}
+        className="single-service-detail-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {link}
+      </a>
+      <br></br>
+    </React.Fragment>
+  ));
+
+  const detailCollection = (
+    <div className="single-service-detail-collection">
+      <h3>{details.detailsTitle}</h3>
+      {links}
+    </div>
+  );
+
   return (
     <div className="single-service">
       <HeaderLogo includeTitleAndButton={false} />
       <div className="single-service-body">
         <div className="single-service-inner-body">
-        {image && <div className="single-service-inner-body-header-wrapper">
-            <img className="single-service-inner-body-header" alt="Palvelua kuvaava kuva" src={process.env.PUBLIC_URL + `/${image}`}/>
-          </div>}
+          {image && (
+            <div className="single-service-inner-body-header-wrapper">
+              <img
+                className="single-service-inner-body-header"
+                alt="Palvelua kuvaava kuva"
+                src={process.env.PUBLIC_URL + `/${image}`}
+              />
+            </div>
+          )}
           <div className="single-service-inner-body-text-wrapper">
-          <div className="single-service-inner-body-text">
-          <h2>{title}</h2>
-          {singleServiceText}
-          <Link className="single-service-home-link" to={"/"}>
-            <p>Palaa takaisin etusivulle</p>
-          </Link>
-          </div>
+            <div className="single-service-inner-body-text">
+              <h2>{title}</h2>
+              {singleServiceText}
+              {details && detailCollection}
+              <Link className="single-service-home-link" to={"/"}>
+                <p>Palaa takaisin etusivulle</p>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
