bdc7938cf4ee71dc9d1de4e50445ff2016ab70c6
Oskar Sundholm <oskar.sundholm@protonmail.com>
2020-07-27 21:35:55 +0300

    added fontawesome library and updated router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,45 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import data from "../../data/data.json";
 import Header from "../Header/Header";
 import Body from "../Body/Body";
 import Footer from "../Footer/Footer";
 import SingleService from "../SingleService/SingleService";
-import NoMatch from "../NoMatch/NoMatch"
+import SingleServiceDetails from "../SingleServiceDetails/SingleServiceDetails";
+import NoMatch from "../NoMatch/NoMatch";
 import ScrollToTop from "./ScrollToTop";
 import "./App.css";
 
+// Initialize FontAwesome library
+library.add(faArrowRight);
+
 const App = () => {
   const routes = data.services.map((service, index) => (
-    <Route exact key={index} path={`/${service.path}`}>
-      <SingleService service={service} />
-    </Route>
+    <Route
+      key={index}
+      path={`/${service.path}`}
+      render={({ match: { url } }) => (
+        <>
+          <Switch>
+            <Route path={`${url}/`} exact>
+              <SingleService service={service} />
+            </Route>
+            {service.details.additionalDetails && (
+              <Route path={`${url}/ratkaisuvaihtoehdot`} exact>
+                <SingleServiceDetails
+                  details={service.details.additionalDetails}
+                />
+              </Route>
+            )}
+            <Route path="*">
+              <NoMatch noMatch={data.noMatch} />
+            </Route>
+          </Switch>
+        </>
+      )}
+    />
   ));
   return (
     <div className="App">
