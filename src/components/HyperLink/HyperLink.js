7c01aa0453402e6fa140a540dd699c1d73d8bf37
Oskar Sundholm <oskar.sundholm@protonmail.com>
2020-07-28 20:00:11 +0300

    added hyperlink component

---
+++ src/components/HyperLink/HyperLink.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./HyperLink.css";
+
+const HyperLink = (props) => (
+  <div className="hyper-link">
+    <FontAwesomeIcon icon="arrow-right" />
+    <Link className="hyper-link" to={props.path}>
+      {props.text}
+    </Link>
+  </div>
+);
+
+export default HyperLink;
