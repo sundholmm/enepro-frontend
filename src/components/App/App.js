ee0f914e9137c78777ada73db7d5537cf28f10cb
Oskar Sundholm <oskar.sundholm@protonmail.com>
2020-10-30 23:02:07 +0200

    added lazy loaded components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
@@ -6,16 +6,29 @@ import data from "../../data/data.json";
 import Header from "../Header/Header";
 import Body from "../Body/Body";
 import Footer from "../Footer/Footer";
-import SingleService from "../SingleService/SingleService";
-import SingleServiceDetails from "../SingleServiceDetails/SingleServiceDetails";
-import NoMatch from "../NoMatch/NoMatch";
 import ScrollToTop from "./ScrollToTop";
+import Loader from "../Loader/Loader";
 import "./App.css";
+
+// Lazy load subroutes
+const NoMatch = lazy(() => import("../NoMatch/NoMatch"));
+const SingleService = lazy(() => import("../SingleService/SingleService"));
+const SingleServiceDetails = lazy(() =>
+  import("../SingleServiceDetails/SingleServiceDetails")
+);
 
 // Initialize FontAwesome library
 library.add(faArrowRight);
 
 const App = () => {
+  const noMatch = (
+    <Route path="*">
+      <Suspense fallback={<Loader />}>
+        <NoMatch noMatch={data.noMatch} />
+      </Suspense>
+    </Route>
+  );
+
   const routes = data.services.map((service, index) => (
     <Route
       key={index}
@@ -24,21 +37,23 @@ const App = () => {
         <>
           <Switch>
             <Route path={`${url}/`} exact>
-              <SingleService service={service} />
+              <Suspense fallback={<Loader />}>
+                <SingleService service={service} />
+              </Suspense>
             </Route>
             {service.details.additionalDetails && (
               <Route path={`${url}/ratkaisuvaihtoehdot`} exact>
-                <SingleServiceDetails
-                  details={service.details.additionalDetails}
-                  path={service.path}
-                  title={service.title}
-                  metaDesc={service.metaDesc}
-                />
+                <Suspense fallback={<Loader />}>
+                  <SingleServiceDetails
+                    details={service.details.additionalDetails}
+                    path={service.path}
+                    title={service.title}
+                    metaDesc={service.metaDesc}
+                  />
+                </Suspense>
               </Route>
             )}
-            <Route path="*">
-              <NoMatch noMatch={data.noMatch} />
-            </Route>
+            {noMatch}
           </Switch>
         </>
       )}
@@ -55,9 +70,7 @@ const App = () => {
             <Footer />
           </Route>
           {routes}
-          <Route path="*">
-            <NoMatch noMatch={data.noMatch} />
-          </Route>
+          {noMatch}
         </Switch>
       </Router>
     </div>
