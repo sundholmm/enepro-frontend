c0fb6732a9e7e2429b5ebee1215d3505534f2d73
sundholmm <oskar.sundholm@protonmail.com>
2020-11-26 18:55:35 +0200

    made webp checks better

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../../data/data.json";
 import HeaderLogo from "./HeaderLogo";
+import supportsWebP from "supports-webp";
 import "./Header.css";
 
-const Header = props => {
+const Header = () => {
   const title = data.company.map((info, index) => (
     <React.Fragment key={index}>
       <h1>{info.name}</h1>
@@ -16,12 +17,22 @@ const Header = props => {
     element.scrollIntoView({
       behavior: "smooth",
       block: "center",
-      inline: "center"
+      inline: "center",
     });
   };
 
+  const [support, setSupport] = useState(true);
+
+  supportsWebP.then((supported) => {
+    if (supported) {
+      setSupport(true);
+    } else {
+      setSupport(false);
+    }
+  });
+
   return (
-    <div className="header">
+    <div className={support ? "header header-webp" : "header header-no-webp"}>
       <div className="header-flex-container">
         <div className="header-img-logo-wrapper">
           <HeaderLogo
--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -1,23 +1,46 @@
 import React, { useState } from "react";
+import supportsWebP from "supports-webp";
 
 const Picture = (props) => {
   const { webp, jpg, png, className, alt, height } = props;
   const [loaded, setLoaded] = useState(false);
+  const [support, setSupport] = useState(true);
   const handleLoaded = () => {
     setLoaded(true);
   };
+
+  supportsWebP.then((supported) => {
+    if (supported) {
+      setSupport(true);
+    } else {
+      setSupport(false);
+    }
+  });
+
   return (
     <>
       <picture style={{ display: loaded ? "block" : "none" }}>
-        <source
-          srcSet={process.env.PUBLIC_URL + `/${webp}`}
-          type="image/webp"
-        />
-        <source srcSet={process.env.PUBLIC_URL + `/${jpg}`} type="image/jpeg" />
-        <source srcSet={process.env.PUBLIC_URL + `/${png}`} type="image/png" />
+        {support && (
+          <source
+            srcSet={process.env.PUBLIC_URL + `/${webp}`}
+            type="image/webp"
+          />
+        )}
+        {jpg && (
+          <source
+            srcSet={process.env.PUBLIC_URL + `/${jpg}`}
+            type="image/jpeg"
+          />
+        )}
+        {png && (
+          <source
+            srcSet={process.env.PUBLIC_URL + `/${png}`}
+            type="image/png"
+          />
+        )}
         <img
           className={className}
-          src={process.env.PUBLIC_URL + `/${jpg}`}
+          src={process.env.PUBLIC_URL + `/${jpg ? jpg : png}`}
           alt={alt}
           onLoad={handleLoaded}
         />
